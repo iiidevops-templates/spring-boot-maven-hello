a53022300bc9f46bff7bc4f2c12e9513f98300d5
mandsan <mandsan@kimo.com>
2022-04-26 14:41:19 +0800

    修復衝突問題

--- app/src/main/java/com/example/springboot/controller/Hello.java
+++ app/src/main/java/com/example/springboot/controller/Hello.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
+public class Hello {
 
 	@RequestMapping("/")
 	public String index() {
